79b445d5b89bc0efc2b03a8d76d4dc94a6690310
Коля <135145397+Kolya4954@users.noreply.github.com>
2026-05-08 12:03:56 +0300

    done

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import {add, minus, podiliti} from "./js/calculator"
+
+
+console.log(minus(15,2));
+console.log(add(233,23033));
+console.log(podiliti(6,2));
+
+
---
+++ src/js/calculator.js
@@ -0,0 +1,12 @@
+export function minus(a, b){
+    return a-b
+}
+
+export function add(a, b){
+    return a+b
+}
+
+
+export function podiliti(a, b){
+    return a/b
+}
